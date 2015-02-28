eac49139ec87887ed57d6ec9d0e50a4ae7cbae00
Justin Martinez <jlmart88@mit.edu>
2015-02-27 19:33:03 -0500

    Updated resources and schedule

--- assets/js/base.js
+++ assets/js/base.js
@@ -16,5 +16,13 @@ $(document).ready(function(){
       callback();
     }, timeout);
   };
+  // For the time now
+  Date.prototype.timeNow = function(){ return ((this.getHours() < 10)?"0":"") + ((this.getHours()>12)?(this.getHours()-12):this.getHours()) +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds() + " " + ((this.getHours()>12)?('PM'):'AM'); };
+
+  $("#currentTime").html(new Date().timeNow());
+
+  setInterval(function() {
+    $("#currentTime").html(new Date().timeNow());
+  }, 1000)
 
 });
